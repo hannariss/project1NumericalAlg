60ae345674fbc265eb6890a021664e593fe66812
hannariss <rissbacher.h@gmail.com>
2024-10-18 11:31:25 +0200

    chore(NN_methods): implement SGD

--- NN_methods.py
+++ NN_methods.py
@@ -32,5 +32,22 @@ class FNN():
         output_FNN = self.sigma(a_2)
         return output_FNN
 
+    def SGD(self, training_data, epochs, mini_batch_size, eta, test_data=None):
+        if test_data: n_test = len(test_data)
+        n = len(training_data)
+        for j in range(epochs):
+            random.shuffle(training_data)
+            mini_batches = [training_data[k:k+mini_batch_size] for k in range(0, n, mini_batch_size)]
+            for mini_batch in mini_batches:
+                grad_initialise = [np.zeros(w.shape) for w in self.weights] #initialise array with shape w for later addition of every gradient
+                for x, y in mini_batch:
+                    gradient_xy = self.backprop(x, y)
+                    gradient_sum = [grad_init+grad_xy for grad_init, grad_xy in zip(grad_initialise, gradient_xy)] #addition of single gradients
+                self.weights = [w-(eta/len(mini_batch))*g for w, g in zip(self.weights, gradient_sum)]
+            if test_data:
+                print "Epoch {0}: {1} / {2}".format(
+                    j, self.evaluate(test_data), n_test)
+            else:
+                print "Epoch {0} complete".format(j)
     
 
