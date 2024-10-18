4c51bdf739834b0a365533ff6de42db5bad7d002
grimmjule <grimmjule262@gmail.com>
2024-10-18 11:31:55 +0200

    chore(NN_methods): add backprop and evaluation

--- NN_methods.py
+++ NN_methods.py
@@ -1,36 +1,74 @@
 import numpy as np
+import random
 
 class FNN():
-    def __init__(self, num_nodes0, num_nodes1, num_nodes2, sigma):
-        self.num_nodes0 = num_nodes0
-        self.num_nodes1 = num_nodes1
-        self.num_nodes2 = num_nodes2
+    def __init__(self, layer_sizes):
+        self.layer_sizes = layer_sizes
         self.T = 2
-        self.sigma = sigma
-
-    def input(self, w, x):
-        """
-        Parameters:
-        w: np.array
-            Matrix shaped |V_(t-1)| x |V_t|
-        x: np.array
-            input vector
-
-        Out:
-        a: np.array
-            array containing input for a layer (|V_(t)|)
-        """
-        a = np.dot(w.T, x)
-        return a
     
     def iteration(self, x_0):
-        w1 = np.zeros(self.num_nodes0, self.num_nodes1)
-        w2 = np.zeros(self.num_nodes1, self.num_nodes2)
-        a_1 = self.input(w1, x_0)
-        o_1 = self.sigma(a_1)
-        a_2 = self.input(w2, o_1)
-        output_FNN = self.sigma(a_2)
-        return output_FNN
+        out = x_0
+        for w in self.weights:
+            input = np.append(out, [1])
+            a = np.dot(w, input)
+            out = self.sigmoid(a)
+        return out
 
+    def sigmoid(self, z):
+        """The sigmoid function."""
+        return 1.0/(1.0+np.exp(-z))
+
+    def sigmoid_prime(self, z):
+        """Derivative of the sigmoid function."""
+        return self.sigmoid(z)*(1-self.sigmoid(z))
     
+    def backprop(self, x, y, weights):
+        grad = [np.zeros(w.shape) for w in weights]
 
+        # forward
+        input = np.append(x, [1])
+        a_vecs = [] # store activations of each layer
+        outs = [x] # store outputs of each layer
+        l = 1 # current layer
+        for w in weights:
+            l += 1
+            a = np.dot(w, input)
+            a_vecs.append(a)
+            out = self.sigma(a)
+            if not l == len(self.layer_sizes): # If we're not in the last layer, append 1 (bias)
+                out = np.append(out, [1])
+            outs.append(out)
+            input = out
+        # backward
+        delta = outs[-1] - y
+        grad[-1] = np.outer((delta * self.sigmoid_prime(a_vecs[-1])), outs[-2])
+        for l in range(1, self.T):
+            a = a_vecs[-l]
+            sp = self.sigmoid_prime(a)
+            delta = np.dot(weights[-l].T, delta * sp)
+            grad[-l-1] = np.outer((delta * self.sigmoid_prime(a_vecs[-l-1])), outs[-l-2])
+        return grad
+
+    def evaluate(self, test_data):
+        """
+        Return the number of test inputs for which the neural network
+        outputs the correct result.
+        
+        Args:
+            test_data: List of tuples (x, y) where
+                    - x is the input to the network
+                    - y is the actual expected output (label)
+        
+        Returns:
+            The count of correct predictions made by the network.
+        """
+        correct_predictions = 0  # Initialize counter for correct classifications
+        
+        # Process each input (x) and label (y) in the test dataset
+        for (x, y) in test_data:
+            output_activations = self.iteration(x)
+            predicted_number = np.argmax(output_activations)
+            if predicted_number == y: # Check if the prediction matches the actual label
+                correct_predictions += 1  # Increment counter if correct
+        
+        return correct_predictions
