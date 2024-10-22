90a511af4e3f980fca8b1616aee40b07ca3da210
grimmjule <grimmjule262@gmail.com>
2024-10-22 10:56:07 +0200

    chore(NN_methods): add docstrings and comments

--- NN_methods.py
+++ NN_methods.py
@@ -3,19 +3,34 @@ import random
 
 class FNN():
     def __init__(self, layer_sizes):
+        """
+        Create feedforward neural netword
+        
+        Args:
+            layer_sizes: np.array, each entry represents one layer and defines its size
+        """
         self.layer_sizes = layer_sizes
-        self.T = 2
+        self.T = len(layer_sizes) - 1
         self.weights = [np.random.randn(y, x) for x, y in zip(layer_sizes[:-1]+np.array(1), layer_sizes[1:])] #weights creates an array containing two arrays: weights for V0 to V1 and weights for V1 to V2
     
-    def sigmoid(self, z):
+    def sigmoid(self, x):
         """The sigmoid function."""
-        return 1.0/(1.0+np.exp(-z))
+        return 1.0/(1.0+np.exp(-x))
 
-    def sigmoid_prime(self, z):
+    def sigmoid_prime(self, x):
         """Derivative of the sigmoid function."""
-        return self.sigmoid(z)*(1-self.sigmoid(z))
+        return self.sigmoid(x)*(1-self.sigmoid(x))
 
-    def prediction(self, x_0):
+    def feedforward(self, x_0):
+        """
+        Computes output of neural network (prediction) by iteratively calculating outputs of each layer
+
+        Args:
+            x_0: np.array, input to the neural network 
+
+        Return: 
+            out: np.array, predicted label corresponding to given input
+        """
         out = x_0
         for w in self.weights:
             input = np.append(out, [1])
@@ -41,33 +56,48 @@ class FNN():
                 print("Epoch {0} complete".format(j + 1))
     
     def backprop(self, x, y):
-        grad = [np.zeros(w.shape) for w in self.weights]
+        """
+        Perform backpropagation to approximate gradient of the loss function.
 
-        # forward
-        input = np.append(x, [1])
-        a_vecs = [] # store activations of each layer
-        outs = [input] # store outputs of each layer
-        l = 1 # current layer
+        Args:
+            x: np.array, input to the neural network 
+            y: np.array, true expected output
+
+        Return:
+            grad: np.array, contains matrices of gradients with respect to the weights of the network (used in SGD)
+            grad_input: np.array, contains gradients with respect to input neurons (used in attack)
+        """
+        grad = [np.zeros(w.shape) for w in self.weights] # initialize array for gradients in shape of weights array
+        input = np.append(x, [1])                        # append bias node
+        a_vecs = []                                      # store activations of each layer
+        outs = [input]                                   # store outputs of each layer
+        l = 1                                            # current layer
+        
+        # Forward
+        # Calculate all activations and outputs of each layer and store them in lists
         for w in self.weights:
             l += 1
             a = np.dot(w, input)
             a_vecs.append(a)
-            out = self.sigmoid(a)
+            out = self.sigmoid(a)    
             if not l == len(self.layer_sizes): # If we're not in the last layer, append 1 (bias)
-                out = np.append(out, [1])
+                out = np.append(out, [1])    
             outs.append(out)
             input = out
-        # backward
+        
+        # Backward
+        # Calculate delta for each layer and from that, the gradients with respect to the weights (grad)
         delta = outs[-1] - y
-        grad[-1] = np.outer((delta * self.sigmoid_prime(a_vecs[-1])), outs[-2])
+        grad[-1] = np.outer((delta * self.sigmoid_prime(a_vecs[-1])), outs[-2]) 
         for l in range(1, self.T):
             a = a_vecs[-l]
             sp = self.sigmoid_prime(a)
-            delta = np.dot(self.weights[-l].T, (delta * sp))[:-1]
+            delta = np.dot(self.weights[-l].T, (delta * sp))[:-1] # Ignore bias node 
             grad[-l-1] = np.outer((delta * self.sigmoid_prime(a_vecs[-l-1])), outs[-l-2])
          
-        # Calculate gradient w.r.t. input (x)
+        # Calculate gradient with respect to input (x)
         grad_input = np.dot(self.weights[0].T, (delta * self.sigmoid_prime(a_vecs[0])))[:-1] 
+        
         return grad, grad_input
     
     def test_accuracy(self, test_data, attack=False, epsilon=0.1):
@@ -79,6 +109,8 @@ class FNN():
             test_data: List of tuples (x, y) where
                     - x is the input to the network
                     - y is the actual expected output (label)
+            attack: boolean, set true when accuracy is tested after attack 
+            epsilon: float, Perturbation magnitude (value range: [0, 1])
         
         Returns:
             The count of correct predictions made by the network.
@@ -88,17 +120,18 @@ class FNN():
         # Process each input (x) and label (y) in the test dataset
         for (x, y) in test_data:
             if attack:
-                x = self.fgsm_attack(x, y, epsilon)
-            output_activations = self.prediction(x)
-            predicted_number = np.argmax(output_activations)
+                x = self.fgsm_attack(x, y, epsilon) # Use perturbed x as input if we perform an attack
+            
+            output_activations = self.feedforward(x)
+            predicted_number = np.argmax(output_activations) 
             if predicted_number == np.argmax(y): # Check if the prediction matches the actual label
-                correct_predictions += 1  # Increment counter if correct
+                correct_predictions += 1
         
         return correct_predictions
     
     def fgsm_attack(self, x, y, epsilon):
         """
-        Perform FGSM attack to generate an adversarial example.
+        Perform FGSM attack to generate an input with perturbation.
         
         Args:
             network: Trained neural network model.
