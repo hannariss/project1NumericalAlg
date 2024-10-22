2f71f31ac65eff537f59a6a317fe99a3d49fb8bb
grimmjule <grimmjule262@gmail.com>
2024-10-22 10:55:34 +0200

    chore(main): add comments

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import NN_methods
 import load_data
 
+# Load data
 training_data, validation_data, test_data = load_data.data_control()
 
+# Train network
 network = NN_methods.FNN([784, 30, 10])
 network.SGD(training_data, 15, 10, 3.0, test_data=test_data)
 
