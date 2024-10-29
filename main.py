6fdc4df937ff5a727bddb41ad253fc3c99b336ce
grimmjule <grimmjule262@gmail.com>
2024-10-29 16:38:10 +0100

    chore(main): adapt to calculation of loss function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import NN_methods
 import load_data
 
@@ -6,8 +8,8 @@ training_data, validation_data, test_data = load_data.process_data()
 
 # Train network
 network = NN_methods.FNN([784, 30, 10])
-network.SGD(training_data, 15, 10, 3.0, test_data=test_data)
+network.SGD(training_data, 30, 10, 3.0, test_data=test_data)
 
 # Attack
-correct_on_attack = network.test_accuracy(test_data, attack=True, epsilon=0.01)
-print(f"\nAfter attack: {correct_on_attack} / {len(test_data)} correct predictions")
+correct_on_attack, loss = network.test_accuracy(test_data, attack=True, epsilon=0.01)
+print(f"\nAfter attack: {correct_on_attack} / {len(test_data)} correct predictions. Loss = {np.round(loss, decimals=3)}")
